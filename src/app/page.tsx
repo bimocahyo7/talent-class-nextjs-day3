af37cc700d760d59d63e2241e1d8a7d2a3769246
bimocahyo7 <bimochayo@gmail.com>
2024-05-20 22:42:18 +0700

    Modify CSS style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import ImageCard from "@/components/card-image";
 
 export default function Home() {
   return (
-    <div className="container py-5">
-      <h1 className="pb-5 text-center font-extrabold">Day 3 - Props NextJS</h1>
+    <div className="container min-h-screen py-5 bg-lime-100">
+      <h1 className="pb-5 text-center text-lg font-extrabold text-slate-700">Day 3 - Props NextJS</h1>
 
-      <div className="flex gap-6 items-center justify-center">
+      <div className="flex gap-8 items-center justify-center flex-wrap">
         <ImageCard name="Abyssinian" image="https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg" />
         <ImageCard name="Bombay" image="https://cdn2.thecatapi.com/images/5iYq9NmT1.jpg" />
         <ImageCard name="Cheetoh" image="https://cdn2.thecatapi.com/images/IFXsxmXLm.jpg" />
         <ImageCard name="Donskoy" image="https://cdn2.thecatapi.com/images/3KG57GfMW.jpg" />
+        <ImageCard name="Aegean" image="https://cdn2.thecatapi.com/images/ozEvzdVM-.jpg" />
+        <ImageCard name="American Curl" image="https://cdn2.thecatapi.com/images/xnsqonbjW.jpg" />
+        <ImageCard name="American Shorthair" image="https://cdn2.thecatapi.com/images/JFPROfGtQ.jpg" />
       </div>
     </div>
   );
--- src/components/card-image.tsx
+++ src/components/card-image.tsx
@@ -7,9 +7,9 @@ interface Cat {
 
 function ImageCard({ name, image }: Cat) {
   return (
-    <div>
-      <Image src={image} alt={name} width={200} height={200} className="w-48 h-48 rounded-xl object-cover" />
-      <h2 className="text-center">{name}</h2>
+    <div className="p-3 rounded-xl bg-indigo-100 border-4 border-indigo-300 transition duration-300 ease-in-out hover:scale-110">
+      <Image src={image} alt={name} width={200} height={200} className="w-40 h-40 rounded-md object-cover" />
+      <h2 className="text-center font-sans font-semibold text-pink-800">{name}</h2>
     </div>
   );
 }
